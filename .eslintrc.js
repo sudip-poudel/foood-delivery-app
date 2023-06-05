a48f1de24b47d5b12106ec0a4b2527fea46f3b10
Sudip Poudel <sudippoudel181@gmail.com>
2023-06-05 19:13:21 +0545

    backend added

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node":true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "warn",
+        "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+      },
+}
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 // const cors = require("cors");
 const mongoDB = require("./db");
-
 const port = 5000;
 mongoDB();
 app.use(express.json());
