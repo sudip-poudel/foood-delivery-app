227fa2f446d64379cd11631e0607c5a59e50b925
Sudip Poudel <sudippoudel181@gmail.com>
2023-06-21 21:58:38 +0545

    change

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,21 @@ const port = 5000;
 mongoDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-With, Content-Type, Accept"
+// 	);
+// 	next();
+// });
 // app.use(cors());
+const cors = require("cors");
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "https://reactmeals-ujly.onrender.com"],
+	})
+);
 
 app.get("/", (req, res) => {
 	res.send("Jelli world");
