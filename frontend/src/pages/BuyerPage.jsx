987b4fc48e912f78ac2f9781c6d7cb1403edfb6f
Sudip <sudippoudel181@gmail.com>
2024-05-17 19:21:02 +0545

    shadch setup

--- frontend/src/pages/BuyerPage.jsx
+++ frontend/src/pages/BuyerPage.jsx
@@ -3,6 +3,13 @@ import Cart from "../components/Cart/Cart";
 import Header from "../components/Layout/Header";
 import Meals from "../components/Meals/Meals";
 import Footer from "../components/Layout/Footer";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/shadcomp/ui/carousel";
 const BuyerPage = () => {
   const [showCart, setShowCart] = useState(false);
   const showCartHandler = () => {
@@ -15,6 +22,17 @@ const BuyerPage = () => {
     <>
       {showCart && <Cart onClose={hideCartHandler} />}
       <Header onClick={showCartHandler} />
+      <div>
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
       <main>
         <Meals />
       </main>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig(() => {
-	return {
-		build: {
-			outDir: "build",
-		},
-		plugins: [react()],
-	};
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "./") }],
+    },
+  };
 });
